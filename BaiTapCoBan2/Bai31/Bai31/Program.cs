e4ed11c781455ec03c3ccd42bb20bfaf5112c419
huutuong2411 <93025125+huutuong2411@users.noreply.github.com>
2022-03-12 14:58:34 +0700

    Bai tap co ban / bai 31

---
+++ BaiTapCoBan2/Bai31/Bai31/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Bai31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long i, j;
+            Console.WriteLine("Bang cuu chuong\n");
+            for (i = 1; i <= 10; ++i)
+            {
+                for (j = 2; j <= 9; ++j)
+                    Console.WriteLine($"| {i} * {j} = {i*j} |");
+                Console.WriteLine("\t");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
