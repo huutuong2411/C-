4a421d56dc59d1bc841acec2ad45177fe7b18855
huutuong2411 <93025125+huutuong2411@users.noreply.github.com>
2022-03-12 14:43:18 +0700

    Bai tap cơ ban 2/ bai 23

---
+++ BaiTapCoBan2/Bai23/Bai23/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Bai23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long n160, sum160;
+            Console.WriteLine("Nhap n: ");
+            n160 = long.Parse(Console.ReadLine());
+            for (int number = 1; number <= n160; number++)
+            {
+                sum160 = 0;
+                for (int i160 = 1; i160 <= number; i160++)
+                    if (number % i160 == 0)
+                        sum160 += i160;
+                if (sum160 / 2.0 == number)
+                    Console.WriteLine($"{number} ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
