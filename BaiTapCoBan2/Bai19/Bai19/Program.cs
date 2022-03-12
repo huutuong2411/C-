67ef071f020f1f3e05c18672b7aa748d19792d3c
huutuong2411 <93025125+huutuong2411@users.noreply.github.com>
2022-03-12 13:59:15 +0700

    Bai tap co ban 2 / bai 19

---
+++ BaiTapCoBan2/Bai19/Bai19/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Bai19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int h160, m160, s160;
+            long time160, hh160, hm160, hs160;
+            Console.WriteLine("Nhap gio, phut, giay [1]: ");
+            h160 = int.Parse(Console.ReadLine());
+            m160 = int.Parse(Console.ReadLine());
+            s160 = int.Parse(Console.ReadLine());
+
+            time160 = 3600 * h160 + 60 * m160 + s160;
+
+            Console.WriteLine("Nhap gio, phut, giay [2]: ");
+            h160 = int.Parse(Console.ReadLine());
+            m160 = int.Parse(Console.ReadLine());
+            s160 = int.Parse(Console.ReadLine());
+
+            time160 -= 3600 * h160 + 60 * m160 + s160;
+            if (time160 < 0) { 
+                time160 = -time160; 
+            }
+
+            hh160 = time160 / 3600;
+            hm160 = (time160 % 3600) / 60;
+            hs160 = (time160 % 3600) % 60;
+
+            Console.WriteLine($"Hieu thoi gian: {hh160} gio, {hm160} phut, {hs160} giay");
+            Console.ReadKey();
+        }
+    }
+}
