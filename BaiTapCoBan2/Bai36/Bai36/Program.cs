9f74f206a7208163233075032259c7d74926cc99
huutuong2411 <93025125+huutuong2411@users.noreply.github.com>
2022-03-12 15:04:25 +0700

    Bai tap co ban 2 / bai 36

---
+++ BaiTapCoBan2/Bai36/Bai36/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Bai36
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long n160, k160, i160, count160;
+            Console.WriteLine("Nhap n: ");
+            n160 = long.Parse(Console.ReadLine());
+            k160 = 2;
+            count160 = 0;
+            while (count160 < n160)
+            { /* vòng lặp kiểm tra k có phải là số nguyên tố */
+                for (i160 = 2; i160 * i160 <= k160; ++i160)
+                    if (k160 % i160 == 0) break;
+                if (i160 * i160 > k160)
+                {
+                    Console.WriteLine($"{k160}");
+                    count160++;
+                }
+                k160++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
