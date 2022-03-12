fdacfa56c880a26fe8a9a3c6745da219dfa1b40d
huutuong2411 <93025125+huutuong2411@users.noreply.github.com>
2022-03-12 14:17:02 +0700

    Bai tap co ban 2 / bai 22

---
+++ BaiTapCoBan2/Bai22/Bai22/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Bai22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            long n160, i160, count160, sum160;
+            Console.WriteLine("Nhap n: ");
+            n160 = long.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap cac uoc so: ");
+            for (count160 = sum160 = 0, i160 = 1; i160 <= n160; ++i160)
+                if (n160 % i160 == 0)
+                {
+                    Console.WriteLine($"{i160}");
+                    count160++;
+                    sum160 += i160;
+                }
+            Console.WriteLine($"\nCo {count160} uoc so, tong la: {sum160}\n");
+
+        }
+    }
+}
