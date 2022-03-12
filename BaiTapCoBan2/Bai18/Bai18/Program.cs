b1891e0c61e0aec3ba2886e96f6cb99aedd0b53f
huutuong2411 <93025125+huutuong2411@users.noreply.github.com>
2022-03-12 13:48:11 +0700

    Bai tap co ban 2 / bai 18

---
+++ BaiTapCoBan2/Bai18/Bai18/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Bai18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long h160, t160, n160;
+            Console.Write("Nhap so gio: ");
+            h160 = long.Parse(Console.ReadLine());
+
+            t160 = h160 / (24 * 7);
+            n160 = (h160 % (24 * 7)) / 24;
+            h160 = (h160 % (24 * 7)) % 24;
+
+            Console.WriteLine($"{t160} tuan, {n160} ngay, {h160} gio");
+            Console.ReadKey();
+        }
+    }
+}
